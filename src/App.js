6c5f8b38d97600d1655122d7fdf101ef0df35f7c
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-06-20 11:41:20 -0400

    makes api request for demo purposes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommentForm from "./components/comments/CommentForm";
 import CommentFeed from "./components/comments/CommentFeed";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet&id=RI0BDz4KRIw`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log("got data from api:", data))
+      .catch((err) => console.warn("something went wrong", err));
+  });
+
   return (
     <div className="App">
       <h1>YouTube</h1>
